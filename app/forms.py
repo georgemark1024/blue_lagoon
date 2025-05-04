4a37ae6da480356eb282f6baff7e18de0247d409
George Mark <144449716+georgemark1024@users.noreply.github.com>
2025-05-04 20:37:57 +0300

    (feat): Add logo and update templates

--- app/forms.py
+++ app/forms.py
@@ -5,12 +5,19 @@ class ClientForm(forms.ModelForm):
     class Meta:
         model = Client
         fields = ['first_name', 'last_name', 'email', 'phone']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
 class ReservationForm(forms.ModelForm):
     class Meta:
         model = Reservation
         fields = ['number_of_rooms', 'check_in', 'check_out']
         widgets = {
-            'check_in': forms.DateInput(attrs={'type': 'date'}),
-            'check_out': forms.DateInput(attrs={'type': 'date'}),
+            'number_of_rooms': forms.NumberInput(attrs={'class': 'form-control'}),
+            'check_in': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'check_out': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
         }
